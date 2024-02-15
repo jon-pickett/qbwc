7a675312fe314f9977c707ca30efd563278fe359
Jonathon Pickett <jpickett76@gmail.com>
2024-02-15 15:42:15 -0500

    Added coder: JSON  to other 2 serializers

--- lib/qbwc/active_record/job.rb
+++ lib/qbwc/active_record/job.rb
@@ -2,8 +2,8 @@ class QBWC::ActiveRecord::Job < QBWC::Job
   class QbwcJob < ActiveRecord::Base
     validates :name, :uniqueness => { :case_sensitive => true }, :presence => true
     serialize :requests, type: Hash, coder: JSON
-    serialize :request_index, type: Hash
-    serialize :data
+    serialize :request_index, type: Hash, coder: JSON
+    serialize :data, coder: JSON
 
     def to_qbwc_job
       QBWC::ActiveRecord::Job.new(name, enabled, company, worker_class, requests, data)
